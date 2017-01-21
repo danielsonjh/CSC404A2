37abdc60626cf4cae5c477939ed68519a2dc5ac1
g3bilode <philip.bilodeau@gmail.com>
2017-01-21 14:01:39 -0500

    Added Scene with timer and gamecontroller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text countDownText;
+    float currentTime = 60f;
+
+	// Use this for initialization
+	void Start () {
+
+        countDownText.text = currentTime.ToString();
+        StartCoroutine(countDown());
+	}
+	
+    IEnumerator countDown()
+    {
+        while (currentTime > 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTime -= 1;
+            countDownText.text = currentTime.ToString();
+        }
+    }
+}
